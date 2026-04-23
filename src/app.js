3856145e0244a5b6e7915cbf7923d0b83fea27ec
Dipak Deshmukh <dipak.deshmukh1911@gmail.com>
2026-04-23 13:28:27 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,23 +2,10 @@ const express = require('express');
 
 const app = express();
 
-
-app.get("/user", (req, res) => {
-    res.send({firstname: "Dipak", lastname: "Deshmukh"});
+app.get("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.params);
+    res.send({firstname : "Dipak" , lastName : "Deshmukh"});
 });
-
-app.post("/user", (req, res) => {
-    res.send('User created!');
-});
-app.delete("/user", (req, res) => {
-    res.send('User deleted!');
-});
-app.get("/user", (req, res) => {
-    res.send('Hello user!');
-})
-app.get("/test", (req, res) => {
-    res.send('Hello test!');
-})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
